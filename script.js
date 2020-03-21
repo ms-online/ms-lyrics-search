ae35f2c70f15ae8509e258412c2468d1bfd8a541
ms-online <27732357@qq.com>
2020-03-21 15:40:42 +0800

    lesson-5

--- script.js
+++ script.js
@@ -10,6 +10,65 @@ const apiURL = "https://api.lyrics.ovh";
 async function searchSongs(term) {
   const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
+  console.log(data);
+
+  showData(data);
+}
+
+// 显示歌曲信息到DOM
+function showData(data) {
+  //   let output = "";
+  //   data.data.forEach(song => {
+  //     output += `
+  //         <li>
+  //         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //         <button class="btn" data-artist ="${song.artist.name}" data-songtitle ="${song.title}">歌词</button>
+  //         </li>
+  //         `;
+  //   });
+
+  //   result.innerHTML = `
+  //     <ul class="songs">
+  //     ${output}
+  //     </ul>
+  //     `;
+
+  result.innerHTML = `
+    <ul class ="songs">
+    ${data.data
+      .map(
+        song => `
+    <li>
+<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+<button class="btn" data-artist ="${song.artist.name}" data-songtitle ="${song.title}">歌词</button>
+</li>
+    `
+      )
+      .join("")}
+    </ul>
+    `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `${
+      data.prev
+        ? `<button class="btn" onclick = "getMoreSongs('${data.prev}')">上一页</button>`
+        : ""
+    }
+          ${
+            data.next
+              ? `<button class="btn" onclick = "getMoreSongs('${data.next}')">下一页</button>`
+              : ""
+          }
+          `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// 获取上一页&下一页歌曲信息
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
 
   showData(data);
 }
